7bfb199bb80b36e2544aacb9a3f5407742ede10b
CarlosMestas <42045414+CarlosMestas@users.noreply.github.com>
2022-07-12 20:00:55 -0500

    AR

--- app/src/main/java/com/viajeros/pe/ui/search/SearchViewModel.java
+++ app/src/main/java/com/viajeros/pe/ui/search/SearchViewModel.java
@@ -10,7 +10,7 @@ public class SearchViewModel extends ViewModel {
 
     public SearchViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is search fragment");
+
     }
 
     public LiveData<String> getText() {
